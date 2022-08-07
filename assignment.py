7dfe7f152cd35f5a0d05e4dcc8bbb1370cbfec0c
Ali fereidooni <110258187+Merni-fereidooni@users.noreply.github.com>
2022-08-07 23:22:52 +0430

    Delete assignment.py

--- assignment.py
+++
@@ -1,8 +0,0 @@
-number = int(float(input("enter number : ")))
-
-fifth_number = number % 100000 // 10000
-second_number = number % 100 // 10
-
-sum = fifth_number + second_number
-
-print("sum of the 2nd and 5th no is %.2f" %sum)
