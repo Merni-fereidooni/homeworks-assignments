8459c11987a63dd92be1a3bf671b938b47500b34
Ali fereidooni <110258187+Merni-fereidooni@users.noreply.github.com>
2022-08-07 23:20:34 +0430

    Add files via upload

---
+++ 2nd week/assignment.py
@@ -0,0 +1,8 @@
+number = int(float(input("enter number : ")))
+
+fifth_number = number % 100000 // 10000
+second_number = number % 100 // 10
+
+sum = fifth_number + second_number
+
+print("sum of the 2nd and 5th no is %.2f" %sum)
