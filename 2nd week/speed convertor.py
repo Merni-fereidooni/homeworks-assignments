97815dbc8836c2a909440b78433f76a0d6df942c
Ali fereidooni <110258187+Merni-fereidooni@users.noreply.github.com>
2022-08-15 08:53:30 +0430

    Update speed convertor.py

--- 2nd week/speed convertor.py
+++ 2nd week/speed convertor.py
@@ -2,4 +2,4 @@ mph_speed = float(input("enter speed in mph : "))
 
 kmh_speed = mph_speed * 1.60934
 
-print("speed of object in kmh is %.2f" %kmh_speed)
+print("speed of object in kmh is :" ,kmh_speed)
