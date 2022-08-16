40700618b9bf84ad740b32ed1a17937287675586
Ali fereidooni <110258187+Merni-fereidooni@users.noreply.github.com>
2022-08-16 09:08:59 +0430

    Update The Area of The Triangle.py

--- 2nd week/The Area of The Triangle.py
+++ 2nd week/The Area of The Triangle.py
@@ -1,6 +1,6 @@
 Base = int(float(input("enter the Base of the triangle : ")))
 Hight = int(float(input("enter the Hight of the triangle : ")))
 
-area = Base * Hight / 2
+area = (Base * Hight) / 2
 
 print("Area of the triangle is :" ,area )
