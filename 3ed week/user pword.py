d260e7a02d208c0f26591666a444acf9b0879c8e
Ali fereidooni <110258187+Merni-fereidooni@users.noreply.github.com>
2022-08-16 10:23:39 +0430

    Add files via upload

---
+++ 3ed week/user pword.py
@@ -0,0 +1,23 @@
+user_name= input('please enter your username :')
+
+
+if user_name == 'shit' :
+    print('username found')
+else :
+    print('username not found')
+    print('username hint : food after digestion')
+
+
+
+
+pword = input('please enter your password :')
+
+
+if pword == 'shithole' :
+    print('Access Granted')
+else :
+    print('Access Denied')
+    print('password hint : where shit comes from')
+   
+
+
