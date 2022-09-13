7851608b3b505487ab7f40981211e7db155cb593
Ali fereidooni <110258187+Merni-fereidooni@users.noreply.github.com>
2022-09-14 01:04:17 +0430

    Delete 6th assignment.palindrome.py

--- 4th week/6th assignment.palindrome.py
+++
@@ -1,9 +0,0 @@
-x = input('enter any number :')
-try:
-    val = int(x)
-    if x == str(x)[:: -1] :
-        print('the given number is PALINDROME')
-    else :
-        print('the given number is not a PALINDROME')
-except ValueError :
-    print('that ain`t a valid number buddy , Try again!')
